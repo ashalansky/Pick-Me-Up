ba8bc5bf3db2d91b4faa0d5829f991fab5aa9281
Amanda Shalansky <ashalansky20@hotmail.com>
2019-08-23 21:21:52 +0000

    creates database users, food_items, checkout_items

---
+++ sql/checkout_items.sql
@@ -0,0 +1,8 @@
+CREATE TABLE checkout_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  food_item_id INT REFERENCES food_items(id) ON DELETE CASCADE,
+  quantity SMALLINT NOT NULL DEFAULT 0,
+  availability BOOLEAN DEFAULT TRUE,
+  order_date DATE DEFAULT NOW()
+  );
